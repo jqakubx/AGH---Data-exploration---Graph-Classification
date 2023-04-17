91b1526a29f2a53386ab372dc6c5e19ce8663ac8
PiotrMakarewicz <makarepio@gmail.com>
2023-04-17 18:02:11 +0200

    remake graph invariants

--- originalPaper/code/baselines.py
+++ originalPaper/code/baselines.py
@@ -1,4 +1,4 @@
-import argparse
+import networkx as nx
 import numpy as np
 
 from graph import function_basis, convert2nx, get_subgraphs, new_norm
@@ -9,8 +9,25 @@ from tunning import merge_features
 
 ### GRAPH INVARIANTS - TODO
 
-def convert_to_vectors_graph_invariants(dataset, graphs, labels):
-    raise NotImplemented()
+def convert_to_vectors_graph_invariants(graphs, labels):
+    vectors = []
+    for i in range(len(graphs)):
+        if i % 50 == 0: 
+            print('#', end='\n')
+        nx_graph = convert2nx(graphs[i], i)
+
+        graph_diameter = nx.diameter(nx_graph)
+        average_shortest_path = nx.average_shortest_path_length(nx_graph)
+        n_nodes = nx_graph.number_of_nodes()
+        node_degrees = [d for _, d in nx_graph.degree()]
+        avg_degree =  sum(node_degrees) / float(n_nodes)
+
+        vectors.append(
+            [graph_diameter, average_shortest_path, n_nodes, avg_degree]
+        )
+    
+    return np.array(vectors).astype(np.float32), np.array(labels)
+
 
 
 ### LDP - original paper baseline
@@ -20,7 +37,8 @@ def convert_to_vectors_ldp(dataset, graphs, labels, hyperparams):
     
     vectors = []
     for i in range(len(graphs)):
-        if i % 50 ==0: print('#'),
+        if i % 50 == 0: 
+            print('#', end='\n')
         gi = convert2nx(graphs[i], i)
         subgraphs = get_subgraphs(gi)
         gi_s = [function_basis(gi, ['deg'], norm_flag=norm_flag) for gi in subgraphs]
